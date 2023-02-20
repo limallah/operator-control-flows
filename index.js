1c2065367b35284abf52058cb1963b34a8ec3c8c
Halima Ibrahim <sadeeyahibraheem@gmail.com>
2023-02-20 17:10:02 +0100

    update

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+// QUESTION 3
+// SCIENCE SUBJECTS - English, Mathematics, Physics, Chemistry, Biology, Technical Drawing 
+// SOCIAL SCIENCE SUBJECTS - English, Mathematics, Accounting, Commerce, Marketing, Geography
+// ART SUBJECTS - English, Mathematics, Government, Economics, Literature, History
+// GENERAL SUBJECTS - English, Mathematics
+
+let classGroup = 'Social Science'
+
+if (classGroup == 'Science Subjects') {
+    console.log('English, Mathematics, Physics, Chemistry, Biology, Technical Drawing');
+}else if (classGroup == 'Art Subjects') {
+    console.log('English, Mathematics, Government, Economics, Literature, History');
+}else if (classGroup == 'Social Science'){
+    console.log('English, Mathematics, Accounting, Commerce, Marketing, Geography')
+}else {
+    console.log('English, Mathematics')
+}
+
+
+
+// QUESTION 5
+
+
 
